c2d3cec2ca7ae50618e671fa60dc58e3f18ca6f9
Pongstr <twiz.tickler@gmail.com>
2023-02-14 11:15:47 +0200

    init: mods

---
+++ lua/lua/config/cmp.lua
@@ -0,0 +1,114 @@
+import({ "cmp", "luasnip", "lspkind", "luasnip/loaders/from_vscode" }, function(modules)
+	local cmp = modules.cmp
+	modules["luasnip/loaders/from_vscode"].lazy_load()
+
+	local check_backspace = function()
+		local col = vim.fn.col(".") - 1
+		return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+	end
+
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				modules.luasnip.lsp_expand(args.body)
+			end,
+		},
+		mapping = {
+			["<C-k>"] = cmp.mapping.select_prev_item(),
+			["<C-j>"] = cmp.mapping.select_next_item(),
+			["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+			["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+			["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+			["<C-y>"] = cmp.config.disable,
+			["<C-e>"] = cmp.mapping({
+				i = cmp.mapping.abort(),
+				c = cmp.mapping.close(),
+			}),
+			["<CR>"] = cmp.mapping.confirm({ select = false }),
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif modules.luasnip.expandable() then
+					modules.luasnip.expand()
+				elseif modules.luasnip.expand_or_jumpable() then
+					modules.luasnip.expand_or_jump()
+				elseif check_backspace() then
+					fallback()
+				else
+					fallback()
+				end
+			end, {
+				"i",
+				"s",
+			}),
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif modules.luasnip.jumpable(-1) then
+					modules.luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end, {
+				"i",
+				"s",
+			}),
+		},
+		formatting = {
+			fields = { "abbr", "kind", "menu" },
+			format = modules.lspkind.cmp_format({
+				mode = "symbol_text",
+				symbol_map = {
+					Copilot = "",
+				},
+				before = function(entry, vim_item)
+					vim_item.menu = ({
+						nvim_lsp = "[LSP]",
+						luasnip = "[Snippet]",
+						buffer = "[Buffer]",
+						path = "[Path]",
+					})[entry.source.name]
+					return vim_item
+				end,
+			}),
+		},
+		sources = {
+			{ name = "nvim_lsp" },
+			{ name = "copilot" },
+			{ name = "luasnip" },
+			{ name = "buffer" },
+			{ name = "path" },
+		},
+		confirm_opts = {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = false,
+		},
+		window = {
+			completion = cmp.config.window.bordered(),
+			documentation = cmp.config.window.bordered(),
+		},
+		experimental = {
+			ghost_text = false,
+			native_menu = false,
+		},
+		performance = {
+			debounce = 150,
+		},
+	})
+
+	cmp.setup.cmdline({ "/", "?" }, {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = {
+			{ name = "buffer" },
+		},
+	})
+
+	cmp.setup.cmdline(":", {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = cmp.config.sources({
+			{ name = "path" },
+		}, {
+			{ name = "cmdline" },
+		}),
+	})
+end)
